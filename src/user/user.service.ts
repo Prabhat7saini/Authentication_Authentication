c1353643ccefaa8e9ba049b2fab146cca140ee62
Prabhat Saini <Prabhat.saini@binmile.com>
2024-09-16 11:19:35 +0530

    soft delete

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
 import { UserRepository } from './repo/user.repository';
 import { UpdateUserDto } from './dto/userDto';
-import { ApiResponse } from 'src/utils/responses/api-response.dto';
-import { ResponseService } from 'src/utils/responses/ResponseService';
-import { Request } from 'express';
+import { ApiResponse } from '../utils/responses/api-response.dto';
+import { ResponseService } from '../utils/responses/ResponseService';
+import { CustomRequest } from '../utils/interface/type';
+import { ERROR_MESSAGES, SUCCESS_MESSAGES } from '../utils/constants/message';
 
 @Injectable()
 export class UserService {
@@ -13,6 +14,20 @@ export class UserService {
         console.log(id, `update`)
         await this.userRepository.updateUser(id, userDate);
 
-        return this.responseService.success('User updated successfully');
+        return this.responseService.success(SUCCESS_MESSAGES.USER_UPDATED_SUCCESSFULLY);
+    }
+
+    async softDeleteUser(@Req() req: CustomRequest): Promise<ApiResponse> {
+        try {
+            const id = req.user.id;
+            const result = await this.userRepository.softDeleteUser(id);
+            if (!result) {
+                return this.responseService.error(ERROR_MESSAGES.USER_DELETION_FAILED, 500)
+            }
+            return this.responseService.success(SUCCESS_MESSAGES.USER_DELETED_SUCCESSFULLY);
+        } catch (error) {
+            console.error('Error during soft delete:', error);
+            return this.responseService.error(ERROR_MESSAGES.USER_DELETION_FAILED, 500);
+        }
     }
 }
